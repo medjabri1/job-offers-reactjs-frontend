cb74cb38ecd1bd882cb3c6a1fe8601a9348afda7
Mohammed <mohammed.jabri2000@gmail.com>
2021-12-18 03:56:44 +0100

    Index page - added loader and background

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route, Link } from "react-router-dom";
+
+import IndexPage from './Components/IndexPage/IndexPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<IndexPage />} />
+          <Route path="/home" element={<h1>Home</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/Components/IndexPage/IndexPage.js
+++ src/Components/IndexPage/IndexPage.js
@@ -1,11 +1,115 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+
+import axios from 'axios';
 
 import './IndexPage.css';
 
+import Login from './Compononets/Login/Login';
+import Signup from './Compononets/Signup/Signup';
+import { useNavigate } from 'react-router-dom';
+
 function IndexPage() {
+
+    // CONSTS & VARS 
+
+    const API_BASE_URL = 'http://localhost:8081/api';
+
+    let navigate = useNavigate();
+
+    // USE STATE HOOK
+
+    let [displayLogin, setDisplayLogin] = useState(false);
+    let [displaySignup, setDisplaySignup] = useState(false);
+    let [displayLoader, setDisplayLoader] = useState(true);
+    let [userLogged, setUserLogged] = useState(false);
+    let [userId, setUserId] = useState(false);
+
+    // USE EFFECT HOOK
+
+    // Change page title when toggling between login / sign up / index
+
+    useEffect(() => {
+        if (displayLogin) {
+            document.title = "Job Offers - Login";
+        } else if (displaySignup) {
+            document.title = "Job Offers - Signup";
+        } else {
+            document.title = "Job Offers - Index";
+        }
+    }, [displayLogin, displaySignup]);
+
+    useEffect(() => {
+        checkLoginStatus();
+        // request_logout();
+    }, []);
+
+    useEffect(() => {
+        checkLoginStatus();
+    }, [userLogged, userId]);
+
+    // HELPER FUNCTIONS
+
+    // Switch login and signup modals
+
+    let gotoSignupIndex = () => {
+        setDisplayLogin(false);
+        setDisplaySignup(true);
+    };
+
+    let gotoLoginIndex = () => {
+        setDisplaySignup(false);
+        setDisplayLogin(true);
+    };
+
+    // Check log status
+
+    let checkLoginStatus = () => {
+        axios.get(`${API_BASE_URL}/user/log-status`, { withCredentials: true })
+            .then(res => {
+                let user_id = res.data.session_user_id;
+                if (user_id != "null") {
+                    setUserId(user_id);
+                    setUserLogged(true);
+                    navigate("/home");
+
+                } else {
+                    setUserLogged(false);
+                }
+                setTimeout(() => {
+                    setDisplayLoader(false);
+                }, 3000);
+            });
+    }
+
+    // LOG OUT REQUEST
+
+    let request_logout = () => {
+
+        axios.post(`${API_BASE_URL}/user/logout`, {}, { withCredentials: true })
+            .then(res => {
+                console.log(res.data);
+            });
+
+    };
+
+    // RETURN INDEX PAGE COMPONENT
 
     return (
         <div className="index-page">
+
+            {/* LOADING */}
+
+            {
+                displayLoader ?
+                    <div className="loader">
+                        <span className="loader-square">
+                            <span className="dot-1"></span>
+                            <span className="dot-2"></span>
+                        </span>
+                    </div>
+                    : null
+            }
 
             {/* HEADER */}
 
@@ -13,37 +117,53 @@ function IndexPage() {
                 <div className="header-container">
                     <div className="header-logo">Job Offers</div>
                     <ul className="header-nav">
-                        <li className="header-nav-link">Sign Up</li>
-                        <li className="header-nav-link">Log In</li>
+                        <li className="header-nav-link" onClick={() => setDisplaySignup(true)} >Sign Up</li>
+                        <li className="header-nav-link" onClick={() => setDisplayLogin(true)} >Log In</li>
                     </ul>
                 </div>
+            </div>
+
+            {/* Background */}
+
+            <div className="background">
+                <img src="https://ieltsrewind.com/wp-content/uploads/2020/06/Describe-a-large-company-that-you-are-interested-in-ielts-cue-card.jpg" alt="Image from web" />
+                {/* <img src="https://blog.thomasnet.com/hubfs/Big%20company%20corporation.jpg" alt="Image from web" /> */}
             </div>
 
             {/* SHOWCASE */}
 
             <div className="showcase">
                 <h2 className="showcase-slogan">Join us today here in <span>-Job Offers-</span></h2>
-                <p className="showcase-description">To stay up with the latest job offers, by thousands of recruiters!</p>
-            </div>
-
-            {/* LOG IN - SIGN IN MODALS */}
-
-            <div className="login-modal modal">
-                <div className="form-container">
-                    <form>
-                        <h2 className="form-title">Log in</h2>
-                        <label htmlFor="login_email_input" className="form-label">Email</label>
-                        <input type="email" className="form-input" placeholder='Type your email here..' />
-
-                        <label htmlFor="login_password_input" className="form-label">Password</label>
-                        <input type="password" className="form-input" placeholder='Type your password here..' />
-
-                        <input type="submit" className="form-input form-submit" value="Log in" />
-
-                        <p className="new-account">Create new account here</p>
-                    </form>
+                {/* <p className="showcase-description">To stay up with the latest job offers, by thousands of recruiters!</p> */}
+                <p className="showcase-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis ullam laboriosam vitae architecto rem fugiat eveniet tempora eius veritatis velit.</p>
+                <div className="action-buttons">
+                    <h2 className="action-button signup" onClick={() => { setDisplaySignup(true) }}>Sign Up</h2>
+                    <h2 className="action-button login" onClick={() => { setDisplayLogin(true) }}>Log In</h2>
                 </div>
             </div>
+
+            {/* LOG IN MODAL */}
+
+            {
+                displayLogin ?
+                    <Login
+                        API_BASE_URL={API_BASE_URL}
+                        gotoSignup={gotoSignupIndex}
+                        closeModal={() => setDisplayLogin(false)}
+                        setUserId={(id) => setUserId(id)}
+                    /> : null
+            }
+
+            {/* SIGN UP MODAL */}
+
+            {
+                displaySignup ?
+                    <Signup
+                        API_BASE_URL={API_BASE_URL}
+                        gotoLogin={gotoLoginIndex}
+                        closeModal={() => setDisplaySignup(false)}
+                    /> : null
+            }
 
         </div>
     )
