1aaa38f3018b96fa51cf7834f3ae1df11a25e266
Mohammed <mohammed.jabri2000@gmail.com>
2021-12-22 01:39:33 +0100

    Profile Page - Get User profile data from backend 90% DONE

--- src/Components/ProfilePage/Components/Profile/Profile.js
+++ src/Components/ProfilePage/Components/Profile/Profile.js
@@ -1,8 +1,85 @@
 import React from 'react'
 
+import { useState, useEffect } from 'react'
+import { useHistory, useParams } from 'react-router-dom';
+
+import axios from 'axios'
+
 import "./Profile.css";
 
-function Profile() {
+function Profile({ loggedUserId }) {
+
+    const API_BASE_URL = 'http://localhost:8081/api';
+    const profile_id = useParams().id;
+
+    // Change page title
+
+    useEffect(() => {
+        document.title = "Profile - Mohammed JABRI"
+        requestProfileData();
+    }, [, loggedUserId]);
+
+    // Use State Hook
+
+    let [currentUserName, setCurrentUserName] = useState("Full name");
+    let [currentUserRole, setCurrentUserRole] = useState("USER");
+    let [currentUserEmail, setCurrentUserEmail] = useState("Email address");
+    let [currentUserPhone, setCurrentUserPhone] = useState("Phone number");
+    let [currentUserLinkedIn, setCurrentUserLinkedIn] = useState("LinkedIn Profile");
+    let [currentUserAge, setCurrentUserAge] = useState("00");
+    let [currentUserPictureURL, setCurrentUserPictureURL] = useState("");
+
+    let [fetchId, setFetchId] = useState(loggedUserId);
+
+    // Get user profile data 
+
+    let requestProfileData = () => {
+
+        profile_id != undefined && setFetchId(profile_id);
+
+        if (profile_id == undefined || profile_id == loggedUserId) {
+            // Profile for logged user
+            getProfileData(loggedUserId);
+        } else {
+            // Profile for another user
+            getProfileData(profile_id);
+        }
+    }
+
+    let getProfileData = (id) => {
+        axios.get(`${API_BASE_URL}/user/id/?id=${id}`, { withCredentials: true })
+            .then(res => {
+
+                let status = res.data.status;
+                let user = res.data.user;
+
+                if (status == "1") {
+                    setCurrentUserName(user.firstName + " " + user.lastName);
+                    setCurrentUserRole(user.role);
+                    setCurrentUserEmail(user.email);
+                    setCurrentUserPhone("STATIC PHONE +212888");
+                    setCurrentUserLinkedIn("STATIC LINKEDIN LINK");
+                    setCurrentUserAge(getAge(user.birthDate));
+                    setCurrentUserPictureURL("STATIC PICTURE LINK");
+                } else {
+                    console.log(res.data.error);
+                }
+            });
+    }
+
+    // Calculate age by birth date
+
+    let getAge = (birth_date_string) => {
+        var today = new Date();
+        var birthDate = new Date(birth_date_string);
+        var age = today.getFullYear() - birthDate.getFullYear();
+        var m = today.getMonth() - birthDate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        return age;
+    };
+
     return (
         <div className="profile-container">
             <div className="profile-content">
@@ -11,29 +88,41 @@ function Profile() {
                 <h2 className="profile-title about-title">About user</h2>
                 <div className="profile-header">
                     <div className="avatar-box">
-                        <img className="user-avatar" src="https://st3.depositphotos.com/4111759/13425/v/600/depositphotos_134255532-stock-illustration-profile-placeholder-male-default-profile.jpg" alt="Profile avatar" />
+                        <img className="user-avatar" src="https://avatars.githubusercontent.com/u/58910558?v=4" alt="Profile avatar" />
                     </div>
                     <div className="infos-box">
                         <div className="info-item">
                             <p className="infos-label">Full name</p>
-                            <h2 className="infos-data user-full-name">Mohammed JABRI</h2>
+                            <h2 className="infos-data user-full-name">{currentUserName}</h2>
+                        </div>
+                        <div className="info-item">
+                            <p className="infos-label">Role</p>
+                            <p className="infos-data">
+                                {currentUserRole}
+                            </p>
                         </div>
                         <div className="info-item">
                             <p className="infos-label">Email</p>
                             <p className="infos-data">
-                                <a href="mailto:mohammed.jabri2000@gmail.com">mohammed.jabri2000@gmail.com</a>
+                                <a href={"mailto:" + currentUserEmail}>{currentUserEmail}</a>
                             </p>
                         </div>
                         <div className="info-item">
                             <p className="infos-label">Phone number</p>
                             <p className="infos-data">
-                                <a href="tel:+212618259699">+212 618-259-699</a>
+                                <a href={"tel:" + currentUserPhone}>{currentUserPhone}</a>
                             </p>
                         </div>
                         <div className="info-item">
                             <p className="infos-label">LinkedIn Profile</p>
                             <p className="infos-data">
-                                <a href="https://www.linkedin.com/in/mjr-1/">Mohammed JABRI</a>
+                                <a href={currentUserLinkedIn} target="_blank">LinkedIn profile link</a>
+                            </p>
+                        </div>
+                        <div className="info-item">
+                            <p className="infos-label">Age</p>
+                            <p className="infos-data">
+                                {currentUserAge}
                             </p>
                         </div>
                     </div>
@@ -41,6 +130,9 @@ function Profile() {
 
                 {/* RESUME SECTION */}
                 <h2 className="profile-title about-title">Resume section</h2>
+
+                <div className="profile-resume">
+                </div>
             </div>
         </div>
     )
---
+++ src/Components/ProfilePage/ProfilePage.js
@@ -0,0 +1,69 @@
+import React from 'react'
+
+import { useState, useEffect } from 'react'
+import { Routes, Route, useNavigate } from "react-router-dom";
+
+import axios from 'axios'
+
+import Header from "../Header/Header";
+import Profile from "./Components/Profile/Profile";
+
+import "./ProfilePage.css";
+
+function ProfilePage() {
+
+    const API_BASE_URL = 'http://localhost:8081/api';
+    let navigate = useNavigate();
+
+    let [displayLoader, setDisplayLoader] = useState(true);
+    let [loggedUserId, setLoggedUserId] = useState(0);
+
+    useEffect(() => {
+        checkLoginStatus();
+    }, [])
+
+    // Check login status
+
+    let checkLoginStatus = () => {
+        axios.get(`${API_BASE_URL}/user/log-status`, { withCredentials: true })
+            .then(res => {
+                let user_id = res.data.session_user_id;
+                if (user_id != "null") {
+                    // USER LOGGED IN
+                    setTimeout(() => {
+                        setDisplayLoader(false);
+                        setLoggedUserId(user_id);
+                    }, 1000);
+                } else {
+                    navigate("/");
+                }
+            });
+    }
+
+    return (
+        <div className="profile-page">
+
+            {/* LOADING */}
+            {
+                displayLoader ?
+                    <div className="loader">
+                        <span className="loader-square">
+                            <span className="dot-1"></span>
+                            <span className="dot-2"></span>
+                        </span>
+                    </div>
+                    : null
+            }
+
+            <Header />
+            <Routes>
+                <Route path="/" element={<Profile loggedUserId={loggedUserId} />} />
+                <Route path="/:id" element={<Profile loggedUserId={loggedUserId} />} />
+                <Route path="/settings" element={<h2>Profile Settings</h2>} />
+                <Route path="/*" element={<h2>Default route for Profile Page</h2>} />
+            </Routes>
+        </div>
+    )
+}
+
+export default ProfilePage
